c10e4a87469ddd2e9ec6e0bea5c107a127da06f9
SeHyun Bang <86397164+lawijdo201@users.noreply.github.com>
2023-03-28 22:23:24 +0900

    패키지 설정

---
+++ src/test/java/gwnucapstone/trafficmanager/TrafficManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gwnucapstone.trafficmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
